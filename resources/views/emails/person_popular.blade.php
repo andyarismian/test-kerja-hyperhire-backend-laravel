e7e5764633c6321a24ae0d2898af309bf7c10de4
Andy Arismianto <andy@kabayan.id>
2025-12-07 12:27:01 +0700

    Fix gitignore and remove cached files

--- resources/views/emails/person_popular.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-    <p>Admin,</p>
-    <p>The person <strong>{{ $person->name }}</strong> (ID: {{ $person->id }}) has reached {{ $person->likes_count }} likes.</p>
-    <p>Location: {{ $person->location }}</p>
-    <p>Regards,<br/>System</p>
-</body>
-</html>
